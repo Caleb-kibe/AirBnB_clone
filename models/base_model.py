6d3c881d61e55ab63766b65a84c5a6816a6e9a88
caleb kibe <kiplagatcaleb2@gmail.com>
2024-03-14 16:54:38 +0300

    added argument persing using kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,22 @@ import uuid
 
 class BaseModel:
     """Represents BaseModel of the Hbnb applicaion"""
-    def __init__(self):
-        """Definition of the constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+    def __init__(self, *args, **kwargs):
+        """Definition of the constructor
+        Args:
+            *Args: Not used
+            **Kwargs (dict): key/value pairs of the attributes
+        """
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    kwargs[k] = datetime.fromisoformat(v)
+
+            self.__dict__.update(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now().isoformat()
+            self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
         """Defines how the object will be
